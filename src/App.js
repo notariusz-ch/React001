411d87c8c77a57a81cbb68943740252cea3838bc
notariusz-ch <48717587+notariusz-ch@users.noreply.github.com>
2023-08-05 18:30:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      Jam jest przycisk
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Witaj na mojej stronie</h1>
+      <MyButton />
+    </div>
+  );
+}
